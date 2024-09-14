9f3beeee440a198f2a442c2ebd942af1679630bf
indra-zen <2450047@student.ppkia.ac.id>
2024-09-14 23:34:04 +0800

    Variables in Kotlin

---
+++ Variables.kt
@@ -0,0 +1,10 @@
+// main function
+fun main() {
+    val name = "Indra" // Constant value
+    var age: Int = 20 // Variable, we can change it's value
+    println(name)
+    println(age)
+    
+    age = 21
+    println(age)
+}
