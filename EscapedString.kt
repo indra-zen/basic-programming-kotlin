924e74beb75ced94a59e9f853709a19652966d41
indra-zen <2450047@student.ppkia.ac.id>
2024-09-15 07:05:16 +0800

    Escaped String in Kotlin

---
+++ EscapedString.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val message = "Kotlin is \"Awesome!\"\n"
+    print(message)
+    val copyright = "Unicode test: \u00A9"
+    print(name)
+}
+ 
+/*
+   output:
+	Kotlin is "Awesome!"
+	Unicode test : ©
+*/
